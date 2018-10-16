69a722ddec1827110cf9805e44de33a23d372d72
fiandimas <kidaliez@gmail.com>
2018-10-16 17:15:16 +0700

    edit users migration file

--- db/migrate/20181016101224_create_users.rb
+++ db/migrate/20181016101224_create_users.rb
@@ -1,7 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :characters
+      t.string :sea
+      t.string :island
+      t.string :location
+      t.string :quote
+      t.string :akuma_no_mi
       t.timestamps
     end
   end
