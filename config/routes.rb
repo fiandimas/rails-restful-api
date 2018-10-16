95cb7c68af62dd8a437d343054d593b19da3a537
fiandimas <kidaliez@gmail.com>
2018-10-16 18:48:10 +0700

    define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 	namespace 'api' do
-		resource :users
+		get 'user' => 'users#index'
+		post 'user' => 'users#create'
+		get 'user/:id' => 'users#show'
+		put 'user/:id' => 'users#update'
+		delete 'user/:id' => 'users#destroy'
 	end
 end
